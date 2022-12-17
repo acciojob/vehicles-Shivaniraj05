c178324cfb1d7becaedc6960a9f3fc1efa1ad0c8
Shivani <shivaniraj7191@gmail.com>
2022-12-17 19:36:09 +0530

    assignment

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -4,7 +4,7 @@ public class Boat implements WaterVehicle{
     String name;
     int capacity;
 
-    Boat(String name, int capacity){
+    public Boat(String name, int capacity){
         this.name = name;
         this.capacity = capacity;
     }
--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -9,6 +9,62 @@ public class Car extends Vehicle {
     private int currentGear;
     private int seats;
 
+    public int getWheels() {
+        return wheels;
+    }
+
+    public void setWheels(int wheels) {
+        this.wheels = wheels;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+
+    public int getGears() {
+        return gears;
+    }
+
+    public void setGears(int gears) {
+        this.gears = gears;
+    }
+
+    public boolean isManual() {
+        return isManual;
+    }
+
+    public void setManual(boolean manual) {
+        isManual = manual;
+    }
+
+    public int getCurrentGear() {
+        return currentGear;
+    }
+
+    public void setCurrentGear(int currentGear) {
+        this.currentGear = currentGear;
+    }
+
+    public int getSeats() {
+        return seats;
+    }
+
+    public void setSeats(int seats) {
+        this.seats = seats;
+    }
+
     public Car(String name, int wheels, int doors, int gears, boolean isManual, String type, int seats) {
         //Hint: Car extends Vehicle
         super(name);
@@ -20,6 +76,7 @@ public class Car extends Vehicle {
         this.seats = seats;
         currentGear = 1;
     }
+
 
     public void changeGear(int newGear){
         this.currentGear = newGear;
