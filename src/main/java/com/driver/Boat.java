0c0a503ac012f5431b0d579e6717dd27457c77b7
Shivani <shivaniraj7191@gmail.com>
2022-12-17 19:26:39 +0530

    assign

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -1,14 +1,21 @@
 package com.driver;
 
 public class Boat implements WaterVehicle{
+    String name;
+    int capacity;
+
+    Boat(String name, int capacity){
+        this.name = name;
+        this.capacity = capacity;
+    }
 
     @Override
     public String getVehicleName() {
-        return "Water Jet";
+        return this.name;
     }
 
     @Override
     public int getVehicleCapacity() {
-        return 15;
+        return this.capacity;
     }
 }
