05ed0efc78310f7b33ad9ae829ef31d0a658da3e
Shivani <shivaniraj7191@gmail.com>
2025-04-18 01:45:22 +0530

    assigment

---
+++ src/main/java/com/driver/Boat.java
@@ -0,0 +1,21 @@
+package com.driver;
+
+public class Boat implements WaterVehicle{
+    private String name;
+    private int capacity;
+
+    public Boat(String name, int capacity){
+        this.name=name;
+        this.capacity=capacity;
+    }
+
+    @Override
+    public String getVehicleName() {
+        return name;
+    }
+
+    @Override
+    public int getVehicleCapacity() {
+        return capacity;
+    }
+}
--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -4,10 +4,11 @@ public class F1 extends Car {
 
     public F1(String name, boolean isManual) {
         //Use arbitrary values for parameters which are not mentioned
+        super(name, 4, 2, 6, isManual, "F1", 1);
     }
 
     public void accelerate(int rate){
-        int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
+        int newSpeed = getCurrentSpeed()+rate; //set the value of new speed by using currentSpeed and rate
         /**
          * speed 0: gear 1
          * speed 1-50: gear 1
@@ -20,10 +21,30 @@ public class F1 extends Car {
 
         if(newSpeed == 0) {
             //Stop the car, set gear as 1
+            stop();
+            changeGear(1);
         }
         //for all other cases, change the gear accordingly
 
         if(newSpeed > 0) {
+            if(newSpeed<=50){
+                changeGear(1);
+            }
+            else if(newSpeed<=100){
+                changeGear(2);
+            }
+            else if(newSpeed<=150){
+                changeGear(3);
+            }
+            else if(newSpeed<=200){
+                changeGear(4);
+            }
+            else if(newSpeed <= 250){
+                changeGear(5);
+            }
+            else{
+                changeGear(6);
+            }
             changeSpeed(newSpeed, getCurrentDirection());
         }
     }
